d71e28d6d3526ff03872f0b5bd26caed24d3692e
Henrik Bjørnskov <henrik@bjrnskov.dk>
2013-01-21 20:43:03 +0100

    use inject method

--- web/index.php
+++ web/index.php
@@ -3,5 +3,7 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = new Skeleton\Application(__DIR__ . '/..', $debug = true);
-$app['twig.path'] = $app['root_dir'] . '/views';
+$app->inject(array(
+    'twig.path' => $app['root_dir'] . '/views',
+));
 $app->run();
