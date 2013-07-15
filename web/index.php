7efe83a2a49ad7c891bfaa2754331604201de932
Henrik Bjørnskov <henrik@bjrnskov.dk>
2013-07-15 16:09:54 +0200

    yaml-example

--- web/index.php
+++ web/index.php
@@ -4,7 +4,7 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $app = new Skeleton\Application(__DIR__ . '/..', $debug = true);
 $app->inject(array(
-    'routing.resource' => 'routing.xml',
+    'routing.resource' => 'routing.yml',
     'twig.path' => $app['root_dir'] . '/views',
 ));
 $app->run();
